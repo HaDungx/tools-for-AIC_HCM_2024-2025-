e18af231b655c57bfa9ea46701ae31a2c4b580d8
Dũng Hà <dunghachi2008@gmail.com>
2024-09-20 22:30:35 +0700

    fix create_csv

--- create_csv.py
+++ create_csv.py
@@ -1,14 +1,14 @@
 import zipfile
 import os
 
-zipfile_name = 'pack1-groupB'                                                                       # Chinh ten file zip
-zipfile_path = os.path.join('E:/AIC_HCM_2024-2025_data/zipped_data/vong1', zipfile_name +'.zip')    # Chinh duong dan den file zip
+zipfile_name = 'query'                                                                       # Chinh ten file zip
+zipfile_path = os.path.join('E:/private', zipfile_name +'.zip')    # Chinh duong dan den file zip
 dest_path = 'view-res/queries'                                                                      # Chinh duong dan den noi mong muon
 
 def extract_zipfile():
     if not os.path.exists(os.path.join(dest_path, zipfile_name)): # include this to not overwrite
         with zipfile.ZipFile(zipfile_path, 'r') as zf:
-            zf.extractall(dest_path)
+            zf.extractall(os.path.join(dest_path, zipfile_name))
         print('Create query dir!')
     else:    
         print('Query dir alr exist!')
@@ -16,6 +16,7 @@ def extract_zipfile():
 
 def create_csv():
     csv_dirpath = os.path.join(dest_path, zipfile_name + '/submisson_data')
+    txt_files= os.listdir(os.path.join(dest_path, zipfile_name))   
     ok = False 
     if not os.path.exists(csv_dirpath): # include this to not overwrite
         os.makedirs(csv_dirpath)                                               # Tao thu muc ten = zipfile_name chua file csv
@@ -25,9 +26,8 @@ def create_csv():
         ok = True
         print('Empty csv dir alr exist!')           
 
-    if ok:
-        txt_files= os.listdir(os.path.join(dest_path, zipfile_name))     # 27 -> 31: Tao file csv ten = queries         
-        for txt_file in txt_files:
+    if ok:   
+        for txt_file in txt_files:                              # 30 -> 33: Tao file csv ten = queries  
             csv_file = txt_file.split('.')[0] + '.csv'
             with open(os.path.join(csv_dirpath, csv_file), 'w'):
                 pass
